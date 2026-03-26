cdfa3b2d88fa34ee5728f1f4ef7d8856bc714d86
Pushpender <147477490+Pushpender-16@users.noreply.github.com>
2026-03-27 00:02:18 +0530

    Add SQL commands for database and user operations
    Added SQL commands to create databases, show databases, and perform CRUD operations on users.

--- SQL1.sql
+++ SQL1.sql
@@ -1,4 +1,23 @@
-create database class1
 
+-- this code to create a table
+create database class1;
 
-create database class2
+-- this code to show a all database or table
+show databases;
+
+-- this code to create a table
+create database class3;
+INSERT INTO  (name, age)
+VALUES ('Alice', 30);
+
+-- insert qurey
+SELECT id, name, age
+FROM users
+WHERE age > 25
+ORDER BY name;
+UPDATE users
+SET age = 31
+WHERE name = 'Alice';
+DELETE FROM users
+WHERE id = 1;
+-- operation in my sql 
